cfa252a34a64b1d2ecb79360a26017f45d3267bb
CoolestNerdIII <basketball4lyfe2@gmail.com>
2014-03-30 01:03:59 -0400

    Initial Commit of TextExtraction CPP Files

---
+++ TextExtraction/TextExtraction/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TextExtraction
+//
+//  Created by Paul Wilson on 3/30/14.
+//  Copyright (c) 2014 Paul Wilson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
